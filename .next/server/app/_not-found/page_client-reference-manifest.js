27ec5e68ad33145960f099de858a93ba486f4d25
Paulo Victor Gomes <pv.gomes89@gmail.com>
2025-06-01 15:06:17 +0200

    remove next

--- .next/server/app/_not-found/page_client-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/_not-found/page"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/Users/pvgomes/dev/TravelTracker/":[],"/Users/pvgomes/dev/TravelTracker/app/_not-found/page":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}}
