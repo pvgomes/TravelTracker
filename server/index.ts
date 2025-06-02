0b98d2a39d0d5d435856cbba598d679a188595c4
Paulo Victor Gomes <pv.gomes89@gmail.com>
2025-06-02 08:56:03 +0200

    fix: remove throw error after response sent

--- server/index.ts
+++ server/index.ts
@@ -46,8 +46,9 @@ app.use((req, res, next) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
-    res.status(status).json({ message });
-    throw err;
+    console.error(err);
+
+    return res.status(status).json({ message });
   });
 
   // importantly only setup vite in development and after
