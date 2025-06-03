37e06a9d2f09dd5ec317d500ec7f355e0cc4576c
Paulo Victor Gomes <pv.gomes89@gmail.com>
2025-06-03 15:50:05 +0200

    new fix: path resolution in production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve("./client/src"),
-      "@shared": path.resolve("./shared"),
-      "@assets": path.resolve("./attached_assets"),
+      "@": path.resolve(__dirname, "client/src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve("./client"),
+  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve("./dist/public"),
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
 });
